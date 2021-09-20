f4e945340d6d4c8746880ed6fb64d7c9514b918a
ruchiragoswami <ruchigoswami8266@gmail.com>
2021-09-19 18:52:00 -0700

    Update scripts.js
    functions added

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -28,10 +28,41 @@ function checkPalin(someString) {
         return false}
 }
 
-console.log(checkPalin("101"));
-checkPalin("super");
-checkPalin("oppo"); 
+// console.log(checkPalin("101"));
+// checkPalin("super");
+// checkPalin("oppo"); 
 
 
 btnCheck.addEventListener("click", checkPalin); 
+
+let date = {
+    day: 2,
+    month: 9,
+    year: 1999
+}
+
+function convertDateToString(date) {
+
+    var dateStr = {day: '' , month: '', year: ''};
+
+    if (date.day < 10){
+        dateStr.day = '0' + date.day;
+    } else {
+        dateStr.day = date.day.toString() ;
+    }
+
+    if (date.month < 10) {
+        dateStr.month = '0' + date.month;
+    } else {
+        dateStr.month = date.month.toString() ; 
+    }
+
+
+    dateStr.year = date.year.toString(); 
+    return dateStr; 
+
+
+}
+
+console.log(convertDateToString(date)); 
 
